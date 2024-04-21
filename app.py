5d6d775359a4948f3b4dfc91e9cfa94d87825abe
Jessica Nadeak <jessica02nadeak@gmail.com>
2024-04-21 22:38:27 +0700

    add ml

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
 import streamlit as st
 import streamlit.components.v1 as stc
 
+from ML import run_ml_app
+
 html_temp= """
             <div style="padding:10px; background-color:#3872fb;border-radius:10px">
-                <h1>Employee Promotion Prediction App</h1>
-                <h4>This website belongs to HR Team</h4>
+                <h1 style="color:white">Employee Promotion Prediction App</h1>
+                <h4 style="color:white">This website belongs to HR Team</h4>
             </div>
             """
 
@@ -31,7 +33,7 @@ def main ():
         print("EDA")
     elif choice == "ML Section":
         #Run ML
-        print("ML App")
+        run_ml_app()
 
 if __name__ == '__main__':
     main()
